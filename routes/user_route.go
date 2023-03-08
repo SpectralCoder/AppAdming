1127ac9928c39a8dda9cc82e69572495ba01ba72
mujahidul <mujahidul@softwrd.ai>
2023-03-09 01:32:44 +0600

    protecting routes

---
+++ routes/user_route.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	controller "appadming/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+// UserRoutes function
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controller.GetUser())
+}
