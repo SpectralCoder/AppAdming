cd8071137588ec1f22b598385f756b48ce7b7122
SpectralCoder <mujahidcse20@gmail.com>
2023-06-02 03:01:02 +0600

    required field updated for organization

--- models/user_model.go
+++ models/user_model.go
@@ -15,7 +15,7 @@ type User struct {
 	Phone           *int               `json:"phone" validate:"required"`
 	Nid_no          *int               `json:"nid_no"`
 	Token           *string            `json:"token"`
-	Organization_id primitive.ObjectID `json:"org_id" validate:"required"`
+	Organization_id primitive.ObjectID `json:"org_id"`
 	Refresh_token   *string            `json:"refresh_token"`
 	Created_at      time.Time          `json:"created_at"`
 	Updated_at      time.Time          `json:"updated_at"`
